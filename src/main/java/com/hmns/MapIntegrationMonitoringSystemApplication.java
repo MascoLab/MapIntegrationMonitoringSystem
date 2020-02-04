b1601fcb6ef9015e5fe20c45226d98d5d01e3f3c
legendein <legendein@gmail.com>
2020-02-04 11:47:58 +0900

    Tomcat Add Able Setting
    톰켓 구동 가능 셋팅

--- src/main/java/com/hmns/MapIntegrationMonitoringSystemApplication.java
+++ src/main/java/com/hmns/MapIntegrationMonitoringSystemApplication.java
@@ -2,12 +2,18 @@ package com.hmns;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MapIntegrationMonitoringSystemApplication {
-
+public class MapIntegrationMonitoringSystemApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure( SpringApplicationBuilder application ) {
+		return application.sources( MapIntegrationMonitoringSystemApplication.class );
+	}
+	
 	public static void main( String[] args ) {
 		SpringApplication.run( MapIntegrationMonitoringSystemApplication.class, args );
 	}
-
 }
